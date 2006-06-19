8cd7cabb0ec0a7b953413cf6bd65d5b326b034b6
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2006-06-19 21:29:15 +0000

    Added tests for singleton methods and class methods.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@35 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -4,90 +4,31 @@ require 'test/unit'
 require 'ruby-prof'
 require 'test_helper'
 
-def method1
-	sleep(1)
-end
-
-def method2
-	sleep(2)
-	method1
-end
+class C1
+  def C1.hello
+  end
   
-def method3
-	sleep(3)
-	method2
-	method1
+  def hello
+  end
 end
 
-# Need to use wall time for this test due to the sleep calls
-RubyProf::clock_mode = RubyProf::GETTIMEOFDAY
+module M1
+  def hello
+  end
+end
+
+class C2
+  include M1
+  extend M1
+end
+
+class C3
+  def hello
+  end
+end
+
 
 class BasicTest < Test::Unit::TestCase
-
-  def test_basic
-    result = RubyProf.profile do
-      method1
-    end
-    
-    assert_equal(1, result.threads.length)
-
-    methods = result.threads.values.first
-    assert_equal(3, methods.length)
-    
-    method = methods['#toplevel']
-    assert_not_nil(method)
-    assert_in_delta(1, method.total_time, 0.02)
-    assert_in_delta(0, method.self_time, 0.02)
-    assert_in_delta(1, method.called, 0.02)
-    assert_equal(0, method.parents.length)
-    assert_equal(1, method.children.length)
-
-    method = methods['Object#method1']
-    assert_not_nil(method)
-    assert_in_delta(1, method.total_time, 0.02)
-    assert_in_delta(0, method.self_time, 0.02)
-    assert_equal(1, method.called)
-    assert_equal(1, method.parents.length)
-    assert_equal(1, method.children.length)
-    
-    sleep = methods['Kernel.sleep']
-    assert_not_nil(sleep)
-    assert_in_delta(1, sleep.total_time, 0.02)
-    assert_in_delta(1, sleep.self_time, 0.02)
-    assert_in_delta(0, sleep.children_time, 0.02)
-    assert_equal(1, sleep.called)
-    assert_equal(1, sleep.parents.length)
-    assert_equal(0, sleep.children.length)
-  end
-  
-  def test_timings
-    result = RubyProf.profile do
-      method3
-    end
-    
-    assert_equal(1, result.threads.length)
-    methods = result.threads.values.first
-    assert_equal(5, methods.length)
-
-    method = methods['#toplevel']
-    assert_not_nil(method)
-    assert_in_delta(7, method.total_time, 0.02)
-    assert_in_delta(0, method.self_time, 0.02)
-    assert_in_delta(7, method.children_time, 0.02)
-    assert_equal(1, method.called)
-    assert_equal(0, method.parents.length)
-    assert_equal(1, method.children.length)
-    
-    method = methods['Object#method3']
-    assert_not_nil(method)
-    assert_in_delta(7, method.total_time, 0.02)
-    assert_in_delta(0, method.self_time, 0.02)
-    assert_in_delta(7, method.children_time, 0.02)
-    assert_equal(1, method.called)
-    assert_equal(1, method.parents.length)
-    assert_equal(3, method.children.length)
-  end
-  
   def test_double_profile
     RubyProf.start
     assert_raise(RuntimeError) do
@@ -106,5 +47,71 @@ class BasicTest < Test::Unit::TestCase
     assert_raise(ArgumentError) do
 			RubyProf.profile
   	end
+  end
+  
+  def test_class_and_instance_methods
+    result = RubyProf.profile do
+	    C1.hello
+	    C1.new.hello
+    end
+  
+    methods = result.threads.values.first
+    
+    # Length should be 5 - one for top level,
+    # one for Class.new, 1 for Object.initialize
+    # one for Class.hello and one for Class.new.hello
+    assert_equal(5, methods.length)
+    
+    # Check class method
+    method1 = methods['<Class:C1>#hello']
+    assert_not_nil(method1)
+    
+    # Check instance method
+    method1 = methods['C1#hello']
+    assert_not_nil(method1)
+  end
+  
+  def test_module_methods
+    result = RubyProf.profile do
+	  	C2.hello
+	    C2.new.hello
+    end
+  
+    methods = result.threads.values.first
+   
+    # Length should be 4 - one for top level,
+    # one for Class.new, 1 for Object.initialize
+    # one for Class.hello  and Class.new.hello
+    # (remember, they are the same methdo so are 
+		#  combined)
+    assert_equal(4, methods.length)
+    
+    # Check class method
+    method1 = methods['M1.hello']
+    assert_not_nil(method1)
+    assert_equal(2, method1.called)
+  end
+  
+  def test_singleton
+    c3 = C3.new
+    
+    class << c3
+    	def hello
+    	end
+  	end
+  
+    result = RubyProf.profile do
+	  	c3.hello
+    end
+  
+    methods = result.threads.values.first
+    
+    # Length should be 2 - one for top level
+    # and one for the singleton method.
+    assert_equal(2, methods.length)
+    
+    # Check singleton method
+    method1 = methods['<Object:C3>#hello']
+    assert_not_nil(method1)
   end
 end
