ae61d2e31969fa27ba40ba5cd123b15f1bffde0a
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2006-06-19 08:15:33 +0000

    Made the test use wall time so it can measure sleeps.  Also changed a few assert_equals to assert_in_deltas
    git-svn-id: http://ruby-prof.rubyforge.org/svn@30 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -19,6 +19,9 @@ def method3
 	method1
 end
 
+# Need to use wall time for this test due to the sleep calls
+RubyProf::clock_mode = RubyProf::GETTIMEOFDAY
+
 class BasicTest < Test::Unit::TestCase
 
   def test_basic
@@ -33,25 +36,25 @@ class BasicTest < Test::Unit::TestCase
     
     method = methods['#toplevel']
     assert_not_nil(method)
-    assert_equal(1, method.total_time)
-    assert_equal(0, method.self_time)
-    assert_equal(1, method.called)
+    assert_in_delta(1, method.total_time, 0.02)
+    assert_in_delta(0, method.self_time, 0.02)
+    assert_in_delta(1, method.called, 0.02)
     assert_equal(0, method.parents.length)
     assert_equal(1, method.children.length)
 
     method = methods['Object#method1']
     assert_not_nil(method)
-    assert_in_delta(1, method.total_time, 0.01)
-    assert_in_delta(0, method.self_time, 0.01)
+    assert_in_delta(1, method.total_time, 0.02)
+    assert_in_delta(0, method.self_time, 0.02)
     assert_equal(1, method.called)
     assert_equal(1, method.parents.length)
     assert_equal(1, method.children.length)
     
     sleep = methods['Kernel.sleep']
     assert_not_nil(sleep)
-    assert_in_delta(1, sleep.total_time, 0.01)
-    assert_in_delta(1, sleep.self_time, 0.01)
-    assert_in_delta(0, sleep.children_time, 0.01)
+    assert_in_delta(1, sleep.total_time, 0.02)
+    assert_in_delta(1, sleep.self_time, 0.02)
+    assert_in_delta(0, sleep.children_time, 0.02)
     assert_equal(1, sleep.called)
     assert_equal(1, sleep.parents.length)
     assert_equal(0, sleep.children.length)
@@ -63,24 +66,23 @@ class BasicTest < Test::Unit::TestCase
     end
     
     assert_equal(1, result.threads.length)
-
     methods = result.threads.values.first
     assert_equal(5, methods.length)
 
     method = methods['#toplevel']
     assert_not_nil(method)
-    assert_in_delta(7, method.total_time, 0.01)
-    assert_in_delta(0, method.self_time, 0.01)
-    assert_in_delta(7, method.children_time, 0.01)
+    assert_in_delta(7, method.total_time, 0.02)
+    assert_in_delta(0, method.self_time, 0.02)
+    assert_in_delta(7, method.children_time, 0.02)
     assert_equal(1, method.called)
     assert_equal(0, method.parents.length)
     assert_equal(1, method.children.length)
     
     method = methods['Object#method3']
     assert_not_nil(method)
-    assert_in_delta(7, method.total_time, 0.01)
-    assert_in_delta(0, method.self_time, 0.01)
-    assert_in_delta(7, method.children_time, 0.01)
+    assert_in_delta(7, method.total_time, 0.02)
+    assert_in_delta(0, method.self_time, 0.02)
+    assert_in_delta(7, method.children_time, 0.02)
     assert_equal(1, method.called)
     assert_equal(1, method.parents.length)
     assert_equal(3, method.children.length)
