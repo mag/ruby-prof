135a4185598570dce212bbb950889d9f22688dba
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2006-06-21 01:40:29 +0000

    Removed some debugging code.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@52 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/prime_test.rb
+++ test/prime_test.rb
@@ -9,14 +9,11 @@ require 'test_helper'
 # --  Tests ----
 class PrimeTest < Test::Unit::TestCase
   def test_consistency
-    GC.start
     result = RubyProf.profile do
 			run_primes
 		end
 		
-		return
-    
-    result.threads.values.each do |methods|
+	  result.threads.values.each do |methods|
 			methods.values.each do |method|
     		check_parent_times(method)
     		check_parent_calls(method)
