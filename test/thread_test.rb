16792edfec5d1053478be8092723d4e8d941ac34
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2007-07-01 20:38:05 +0000

    Added a sleep test.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@90 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/thread_test.rb
+++ test/thread_test.rb
@@ -7,26 +7,40 @@ require 'test_helper'
 
 # --  Tests ----
 class ThreadTest < Test::Unit::TestCase
+  def test_thread_timings
+    RubyProf.start
+    
+    sleep(2)    
+
+    thread = Thread.new do
+      sleep(2)
+    end
+    
+    thread.join
+    
+    result = RubyProf.stop
+  end
+
   def test_thread
     RubyProf.start
     
     begin
       status = Timeout::timeout(2) do
-      	while true
-        	next
-      	end
-    	end
-   	rescue Timeout::Error
-   	end
+        while true
+          next
+        end
+      end
+    rescue Timeout::Error
+    end
    
     result = RubyProf.stop
     
     result.threads.values.each do |methods|
-			methods.values.each do |method|
-    		check_parent_times(method)
-    		check_parent_calls(method)
-    		check_child_times(method)		
-			end
-  	end
+      methods.values.each do |method|
+        check_parent_times(method)
+        check_parent_calls(method)
+        check_child_times(method)   
+      end
+    end
   end
 end
