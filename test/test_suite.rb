912c25ace8ba8800c6d9f2ac9f4fca8d405dbee6
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2007-07-03 04:48:09 +0000

    Hooking the unit test framework breaks the other unit tests, so comment out.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@137 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/test_suite.rb
+++ test/test_suite.rb
@@ -6,9 +6,13 @@ require 'duplicate_names_test'
 require 'measure_mode_test'
 require 'module_test'
 require 'prime_test'
-require 'profile_unit_test'
 require 'printers_test'
 require 'recursive_test'
 require 'singleton_test'
 require 'thread_test'
 require 'timing_test'
+
+# Can't use this one here cause it breaks
+# the rest of the unit tets (Ruby Prof gets
+# starte twice).
+#require 'profile_unit_test'
