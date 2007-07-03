a4dd6728329be3f79a8de891fc15c7729d92f196
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2007-07-03 04:52:47 +0000

    Fix failing test.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@138 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/module_test.rb
+++ test/module_test.rb
@@ -7,15 +7,18 @@ require 'test_helper'
 
 module Foo
   def Foo::hello
+    sleep(0.5)
   end
 end
 
 module Bar
   def Bar::hello
+    sleep(0.5)
     Foo::hello
   end
   
   def hello
+    sleep(0.5)
     Bar::hello
   end
 end
@@ -32,18 +35,21 @@ class ModuleTest < Test::Unit::TestCase
     methods = methods.sort.reverse
       
     # Length should be 4
-    assert_equal(4, methods.length)
+    assert_equal(5, methods.length)
     
     method = methods[0]
     assert_equal('#toplevel', method.name)
     
     method = methods[1]
-    assert_equal('<Module::Bar>#hello', method.name)
+    assert_equal('Kernel#sleep', method.name)
     
     method = methods[2]
     assert_equal('Bar#hello', method.name)
     
     method = methods[3]
+    assert_equal('<Module::Bar>#hello', method.name)
+    
+    method = methods[4]
     assert_equal('<Module::Foo>#hello', method.name)
   end 
 end
