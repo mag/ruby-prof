d2f8fa0d73824b3d1ed8de772c40884d849b7352
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2007-07-15 21:54:18 +0000

    Fix up failing line number tests.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@208 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/line_number_test.rb
+++ test/line_number_test.rb
@@ -32,38 +32,38 @@ class LineNumbersTest < Test::Unit::TestCase
     assert_equal(3, methods.length)
     
     method = methods[0]
-    assert_equal('LineNumbers#test_function_line_no', method.full_name)
+    assert_equal('LineNumbersTest#test_function_line_no', method.full_name)
     assert_equal(28, method.line)
     
     method = methods[1]
-    assert_equal('Foo#method2', method.full_name)
-    assert_equal(13, method.line)
+    assert_equal('LineNumbers#method1', method.full_name)
+    assert_equal(9, method.line)
     
     method = methods[2]
-    assert_equal('Foo#method1', method.full_name)
-    assert_equal(9, method.line)
+    assert_equal('LineNumbers#method2', method.full_name)
+    assert_equal(13, method.line)
   end
   
   def test_c_function
-    foo = Foo.new
+    numbers = LineNumbers.new
     
     result = RubyProf.profile do
-      foo.method3
+      numbers.method3
     end
 
     methods = result.threads.values.first.sort.reverse
     assert_equal(3, methods.length)
     
     method = methods[0]
-    assert_equal('LineNumbers#test_c_function', method.full_name)
+    assert_equal('LineNumbersTest#test_c_function', method.full_name)
     assert_equal(51, method.line)
     
     method = methods[1]
-    assert_equal('Kernel#sleep', method.full_name)
-    assert_equal(0, method.line)
+    assert_equal('LineNumbers#method3', method.full_name)
+    assert_equal(17, method.line)
     
     method = methods[2]
-    assert_equal('Foo#method3', method.full_name)
-    assert_equal(17, method.line)
+    assert_equal('Kernel#sleep', method.full_name)
+    assert_equal(0, method.line)
   end
 end
