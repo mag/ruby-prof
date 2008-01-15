b36c26e0427ccfe33818047f8e73a369929ee84d
cfis <cfis@5fb4a865-4516-0410-ae3b-a609ddf12040>
2008-01-15 18:59:38 +0000

    Patch from Michael Granger.  See: http://rubyforge.org/tracker/index.php?func=detail&aid=17117&group_id=1814&atid=7062
    git-svn-id: http://ruby-prof.rubyforge.org/svn@220 5fb4a865-4516-0410-ae3b-a609ddf12040

--- test/printers_test.rb
+++ test/printers_test.rb
@@ -7,24 +7,68 @@ require 'test_helper'
 
 # --  Tests ----
 class PrintersTest < Test::Unit::TestCase
-  def test_printers
-    result = RubyProf.profile do
+  
+  def setup
+    @result = RubyProf.profile do
       run_primes
     end
+  end
     
-    printer = RubyProf::FlatPrinter.new(result)
+  def test_printers
+    printer = RubyProf::FlatPrinter.new(@result)
     printer.print(STDOUT)
     
-    printer = RubyProf::GraphHtmlPrinter.new(result)
+    printer = RubyProf::GraphHtmlPrinter.new(@result)
     printer.print
     
-    printer = RubyProf::GraphPrinter.new(result)
+    printer = RubyProf::GraphPrinter.new(@result)
     printer.print
     
-    printer = RubyProf::CallTreePrinter.new(result)
+    printer = RubyProf::CallTreePrinter.new(@result)
     printer.print(STDOUT)
 
     # we should get here
     assert(true)
   end
+
+  def test_flatprinter_duckfriendliness
+    output = ''
+    
+    printer = RubyProf::FlatPrinter.new(@result)
+    assert_nothing_raised { printer.print( output ) }
+    
+    assert_match( /Thread ID: \d+/i, output )
+    assert_match( /Total: \d+\.\d+/i, output )
+    assert_match( /Object#run_primes/i, output )
+  end
+    
+  def test_graphhtmlprinter_duckfriendliness
+    output = ''
+    printer = RubyProf::GraphHtmlPrinter.new(@result)
+    assert_nothing_raised { printer.print(output) }
+
+    assert_match( /DTD HTML 4\.01/i, output )
+    assert_match( %r{<th>Total Time</th>}i, output )
+    assert_match( /Object#run_primes/i, output )
+  end
+    
+  def test_graphprinter_duckfriendliness
+    output = ''
+    printer = RubyProf::GraphPrinter.new(@result)
+    assert_nothing_raised { printer.print(output) }
+
+    assert_match( /Thread ID: \d+/i, output )
+    assert_match( /Total Time: \d+\.\d+/i, output )
+    assert_match( /Object#run_primes/i, output )
+  end
+    
+  def test_calltreeprinter_duckfriendliness
+    output = ''
+    printer = RubyProf::CallTreePrinter.new(@result)
+    assert_nothing_raised { printer.print(output) }
+
+    assert_match( /fn=Object::find_primes/i, output )
+    assert_match( /events: process_time/i, output )
+  end
+
 end
