02cb50d120ed011c8862de7a6be84b5c9c753692
Jeremy Kemper <jeremy@bitsweat.net>
2008-06-12 11:35:53 -0700

    Support multiple :measure_modes

--- lib/ruby-prof/profile_test.rb
+++ lib/ruby-prof/profile_test.rb
@@ -3,31 +3,33 @@ require 'ruby-prof'
 
 module RubyProf
   module Test
-    PROFILE_OPTIONS = {:measure_mode => RubyProf::PROCESS_TIME,
+    PROFILE_OPTIONS = {:measure_modes => [RubyProf::PROCESS_TIME],
                        :count => 10,
                        :printers => [RubyProf::FlatPrinter,
                                      RubyProf::GraphHtmlPrinter],
                        :min_percent => 5,
                        :output_dir => Dir.pwd}
-    
+
 
     def self.included(base)
       base.class_eval do
         include InstanceMethods
       end
     end
-  
+
     module InstanceMethods
       def run(result)
         yield(self.class::STARTED, name)
         @_result = result
         run_warmup
-        data = run_profile
-        report(data)
-        result.add_run
+        PROFILE_OPTIONS[:measure_modes].each do |measure_mode|
+          data = run_profile(measure_mode)
+          report(data, measure_mode)
+          result.add_run
+        end
         yield(self.class::FINISHED, name)
       end
-      
+
       def run_test
         begin
           setup
@@ -46,42 +48,42 @@ module RubyProf
           end
         end
       end
-      
+
       def run_warmup
         # Warmup
         puts "******* #{method_name} *******"
         puts "Warm up:"
-      
-        bench = run_test do
-          Benchmark.realtime do
+
+        run_test do
+          bench = Benchmark.realtime do
             __send__(@method_name)
-          end   
+          end
+          print "%.2f seconds\n\n" % bench
         end
-        print "%.2f seconds\n\n" % bench
       end
-    
-      def run_profile
+
+      def run_profile(measure_mode)
         # Now run
         puts "Running:"
-        RubyProf.measure_mode = PROFILE_OPTIONS[:measure_mode]
-        
+        RubyProf.measure_mode = measure_mode
+
         PROFILE_OPTIONS[:count].times do |i|
-          run_test do 
+          run_test do
             begin
               print i % 10 == 0 ? 'x' : '.'
               STDOUT.flush
               GC.disable
-            
+
               RubyProf.resume do
                 __send__(@method_name)
               end
             ensure
               GC.enable
-            end                             
+            end
           end
-        end                     
+        end
         STDOUT << "\n"
-      
+
         data = RubyProf.stop
         bench = data.threads.values.inject(0) do |total, method_infos|
           top = method_infos.sort.last
@@ -92,42 +94,42 @@ module RubyProf
         data
       end
 
-      def report(data)
+      def report(data, measure_mode)
         PROFILE_OPTIONS[:printers].each do |printer_klass|
           printer = printer_klass.new(data)
-      
+
           # Open the file
-          file_name = report_name(printer)
-      
+          file_name = report_name(printer, measure_mode)
+
           File.open(file_name, 'wb') do |file|
             printer.print(file, PROFILE_OPTIONS)
           end
         end
       end
-      
-      def report_name(printer)
+
+      def report_name(printer, measure_mode)
         # The report name is:
         #   test_name + measure_mode + report_type
         output_dir = PROFILE_OPTIONS[:output_dir]
-      
-        path = case RubyProf.measure_mode
+
+        path = case measure_mode
           when RubyProf::PROCESS_TIME
-            File.join(output_dir, "#{method_name}_process")       
+            File.join(output_dir, "#{method_name}_process")
           when RubyProf::WALL_TIME
-            File.join(output_dir, "#{method_name}_wall")       
+            File.join(output_dir, "#{method_name}_wall")
           when RubyProf::MEMORY
-            File.join(output_dir, "#{method_name}_memory")       
+            File.join(output_dir, "#{method_name}_memory")
           when RubyProf::ALLOCATIONS
             File.join(output_dir, "#{method_name}_allocations")
         end
-          
+
         case printer
           when RubyProf::FlatPrinter
-            path += "_flat.txt"       
+            path += "_flat.txt"
           when RubyProf::GraphPrinter
-            path += "_graph.txt"       
+            path += "_graph.txt"
           when RubyProf::GraphHtmlPrinter
-            path += "_graph.html"       
+            path += "_graph.html"
           when RubyProf::CallTreePrinter
             path += "_tree.txt"
           else
@@ -136,4 +138,4 @@ module RubyProf
       end
     end
   end
-end 
+end
