31cdd82e9655d79e784601abfc686827b3bf04cc
bitsweat <bitsweat@5fb4a865-4516-0410-ae3b-a609ddf12040>
2008-06-28 07:21:37 +0000

    RubyProf::Test supports multiple measurement modes, has more compact feedback, and reports measurements in appropriate units.
    git-svn-id: http://ruby-prof.rubyforge.org/svn@262 5fb4a865-4516-0410-ae3b-a609ddf12040

--- lib/ruby-prof/profile_test.rb
+++ lib/ruby-prof/profile_test.rb
@@ -3,137 +3,137 @@ require 'ruby-prof'
 
 module RubyProf
   module Test
-    PROFILE_OPTIONS = {:measure_mode => RubyProf::PROCESS_TIME,
-                       :count => 10,
-                       :printers => [RubyProf::FlatPrinter,
-                                     RubyProf::GraphHtmlPrinter],
-                       :min_percent => 5,
-                       :output_dir => Dir.pwd}
-    
+    PROFILE_OPTIONS = {
+      :measure_modes => [RubyProf::PROCESS_TIME],
+      :count => 10,
+      :printers => [RubyProf::FlatPrinter, RubyProf::GraphHtmlPrinter],
+      :min_percent => 0.05,
+      :output_dir => Dir.pwd }
 
-    def self.included(base)
-      base.class_eval do
-        include InstanceMethods
-      end
-    end
-  
-    module InstanceMethods
-      def run(result)
-        yield(self.class::STARTED, name)
-        @_result = result
-        run_warmup
-        data = run_profile
-        report(data)
+    def run(result)
+      return if @method_name.to_s == "default_test"
+
+      yield(self.class::STARTED, name)
+      @_result = result
+      run_warmup
+      PROFILE_OPTIONS[:measure_modes].each do |measure_mode|
+        data = run_profile(measure_mode)
+        report_profile(data, measure_mode)
         result.add_run
-        yield(self.class::FINISHED, name)
       end
-      
-      def run_test
+      yield(self.class::FINISHED, name)
+    end
+
+    def run_test
+      begin
+        setup
+        yield
+      rescue ::Test::Unit::AssertionFailedError => e
+        add_failure(e.message, e.backtrace)
+      rescue StandardError, ScriptError
+        add_error($!)
+      ensure
         begin
-          setup
-          yield
+          teardown
         rescue ::Test::Unit::AssertionFailedError => e
           add_failure(e.message, e.backtrace)
         rescue StandardError, ScriptError
           add_error($!)
-        ensure
-          begin
-            teardown
-          rescue ::Test::Unit::AssertionFailedError => e
-            add_failure(e.message, e.backtrace)
-          rescue StandardError, ScriptError
-            add_error($!)
-          end
-        end
-      end
-      
-      def run_warmup
-        # Warmup
-        puts "******* #{method_name} *******"
-        puts "Warm up:"
-      
-        bench = run_test do
-          Benchmark.realtime do
-            __send__(@method_name)
-          end   
-        end
-        print "%.2f seconds\n\n" % bench
-      end
-    
-      def run_profile
-        # Now run
-        puts "Running:"
-        RubyProf.measure_mode = PROFILE_OPTIONS[:measure_mode]
-        
-        PROFILE_OPTIONS[:count].times do |i|
-          run_test do 
-            begin
-              print i % 10 == 0 ? 'x' : '.'
-              STDOUT.flush
-              GC.disable
-            
-              RubyProf.resume do
-                __send__(@method_name)
-              end
-            ensure
-              GC.enable
-            end                             
-          end
-        end                     
-        STDOUT << "\n"
-      
-        data = RubyProf.stop
-        bench = data.threads.values.inject(0) do |total, method_infos|
-          top = method_infos.sort.last
-          total += top.total_time
-          total
-        end
-        puts "%.2f seconds\n\n" % bench
-        data
-      end
-
-      def report(data)
-        PROFILE_OPTIONS[:printers].each do |printer_klass|
-          printer = printer_klass.new(data)
-      
-          # Open the file
-          file_name = report_name(printer)
-      
-          File.open(file_name, 'wb') do |file|
-            printer.print(file, PROFILE_OPTIONS)
-          end
-        end
-      end
-      
-      def report_name(printer)
-        # The report name is:
-        #   test_name + measure_mode + report_type
-        output_dir = PROFILE_OPTIONS[:output_dir]
-      
-        path = case RubyProf.measure_mode
-          when RubyProf::PROCESS_TIME
-            File.join(output_dir, "#{method_name}_process")       
-          when RubyProf::WALL_TIME
-            File.join(output_dir, "#{method_name}_wall")       
-          when RubyProf::MEMORY
-            File.join(output_dir, "#{method_name}_memory")       
-          when RubyProf::ALLOCATIONS
-            File.join(output_dir, "#{method_name}_allocations")
-        end
-          
-        case printer
-          when RubyProf::FlatPrinter
-            path += "_flat.txt"       
-          when RubyProf::GraphPrinter
-            path += "_graph.txt"       
-          when RubyProf::GraphHtmlPrinter
-            path += "_graph.html"       
-          when RubyProf::CallTreePrinter
-            path += "_tree.txt"
-          else
-            raise(RuntimeError, "Unknown printer class: ", printer)
         end
       end
     end
+
+    def run_warmup
+      print "\n#{self.class.name}##{method_name}"
+
+      run_test do
+        bench = Benchmark.realtime do
+          __send__(@method_name)
+        end
+        puts " (%.2fs warmup)" % bench
+      end
+    end
+
+    def run_profile(measure_mode)
+      RubyProf.measure_mode = measure_mode
+
+      print '  '
+      PROFILE_OPTIONS[:count].times do |i|
+        run_test do
+          begin
+            print '.'
+            $stdout.flush
+            GC.disable
+
+            RubyProf.resume do
+              __send__(@method_name)
+            end
+          ensure
+            GC.enable
+          end
+        end
+      end
+
+      data = RubyProf.stop
+      bench = data.threads.values.inject(0) do |total, method_infos|
+        top = method_infos.sort.last
+        total += top.total_time
+        total
+      end
+
+      puts "\n  #{measure_mode_name(measure_mode)}: #{format_profile_total(bench, measure_mode)}\n"
+
+      data
+    end
+
+    def format_profile_total(total, measure_mode)
+      case measure_mode
+        when RubyProf::PROCESS_TIME, RubyProf::WALL_TIME
+          "%.2f seconds" % total
+        when RubyProf::MEMORY
+          "%.2f kilobytes" % total
+        when RubyProf::ALLOCATIONS
+          "%d allocations" % total
+        else
+          "%.2f #{measure_mode}"
+      end
+    end
+
+    def report_profile(data, measure_mode)
+      PROFILE_OPTIONS[:printers].each do |printer_klass|
+        printer = printer_klass.new(data)
+
+        # Open the file
+        file_name = report_filename(printer, measure_mode)
+
+        File.open(file_name, 'wb') do |file|
+          printer.print(file, PROFILE_OPTIONS)
+        end
+      end
+    end
+
+    # The report filename is test_name + measure_mode + report_type
+    def report_filename(printer, measure_mode)
+      suffix =
+        case printer
+          when RubyProf::FlatPrinter; 'flat.txt'
+          when RubyProf::GraphPrinter; 'graph.txt'
+          when RubyProf::GraphHtmlPrinter; 'graph.html'
+          when RubyProf::CallTreePrinter; 'tree.txt'
+          else printer.to_s.downcase
+        end
+
+      "#{PROFILE_OPTIONS[:output_dir]}/#{method_name}_#{measure_mode_name(measure_mode)}_#{suffix}"
+    end
+
+    def measure_mode_name(measure_mode)
+      case measure_mode
+        when RubyProf::PROCESS_TIME; 'process_time'
+        when RubyProf::WALL_TIME; 'wall_time'
+        when RubyProf::MEMORY; 'memory'
+        when RubyProf::ALLOCATIONS; 'allocations'
+        else "measure#{measure_mode}"
+      end
+    end
   end
-end 
+end
